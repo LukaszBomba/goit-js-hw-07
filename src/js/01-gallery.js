43a7384a5868745f82cf9805420661c5980fb783
LukaszBomba <101752120+LukaszBomba@users.noreply.github.com>
2022-07-27 00:39:39 +0200

    zmiany w kodzie

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,30 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 
-const galleryItem = galleryItems.map((galleryItem) => `<div class="gallery__item">
-<a class="gallery__link" href="${galleryItem.original}">
-  <img
-    class="gallery__image"
-    src="${galleryItem.preview}"
-    data-source= "${galleryItem.original}" 
-    alt="${galleryItem.description}"
-  />
-</a>
-</div>`).join("");
+const galleryPic = galleryItems.map((galleryPic) => 
+`<div class="gallery__item">
+<a class="gallery__link" href= "${galleryPic.original}">
 
-gallery.innerHTML = galleryItem;
+  <img
+    class= "gallery__image"
+    src= "${galleryPic.preview}"
+    data-source= "${galleryPic.original}"
+    alt= "${galleryPic.description}"
+  />
+
+</a>
+
+</div>`).join(""); 
+
+gallery.innerHTML = galleryPic;
 
 gallery.addEventListener("click", (event) => {
     event.preventDefault();
 
     const dataImg = event.target.getAttribute("data-source");
+
     const instance = basicLightbox.create(`
-    <img src="${dataImg}" width="800" height="600">`);
-    
-        onShow: (instance) => {
+    <img src="${dataImg}" alt="${altImg}" />
+`, {
+            onShow: (instance) => {
             document.addEventListener("keydown", (event) => {
-            if (event.key === "escape)") instance.close();});
-        }
+                  if (event.key === "Escape") instance.close();
+            })
+      }
+});
     
 instance.show();
 });
