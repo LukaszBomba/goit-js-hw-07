b9daf69657d8598b0728219c036c624a835bd306
LukaszBomba <101752120+LukaszBomba@users.noreply.github.com>
2022-07-26 22:55:40 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -15,6 +15,19 @@ const galleryItem = galleryItems.map((galleryItem) => `<div class="gallery__item
 
 gallery.innerHTML = galleryItem;
 
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
 
+    const dataImg = event.target.getAttribute("data-source");
+    const instance = basicLightbox.create(`
+    <img src="${dataImg}" width="800" height="600">`);
+    
+        onShow: (instance) => {
+            document.addEventListener("keydown", (event) => {
+            if (event.key === "escape)") instance.close();});
+        }
+    
+instance.show();
+});
 
 console.log(galleryItems);
